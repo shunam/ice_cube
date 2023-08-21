6a0e557cb326d8e739e2119dc4a4fbfda8c18645
hendrik <hendrik.kustanto@zvoove.com>
2023-08-21 13:09:49 +0700

    fix i18n conflict

--- lib/ice_cube/i18n.rb
+++ lib/ice_cube/i18n.rb
@@ -1,15 +1,12 @@
-require "ice_cube/null_i18n"
+require 'ice_cube/null_i18n'
 
 module IceCube
   module I18n
-    LOCALES_PATH = File.expand_path(File.join("..", "..", "..", "config", "locales"), __FILE__)
 
-    def self.t(*args, **kwargs)
-      backend.t(*args, **kwargs)
-    end
+    LOCALES_PATH = File.expand_path(File.join('..', '..', '..', 'config', 'locales'), __FILE__)
 
-    def self.l(*args, **kwargs)
-      backend.l(*args, **kwargs)
+    class << self
+      delegate :t, :l, to: :backend
     end
 
     def self.backend
@@ -17,7 +14,10 @@ module IceCube
     end
 
     def self.detect_backend!
-      ::I18n.load_path += Dir[File.join(LOCALES_PATH, "*.yml")]
+      temp_load_path = I18n.load_path
+      rails_i18n_index = temp_load_path.index { |item| item =~ /rails_i18n/ } || -1
+      temp_load_path.insert(rails_i18n_index, *Dir[File.join(LOCALES_PATH, "*.yml")])
+      ::I18n.load_path = temp_load_path
       ::I18n
     rescue NameError
       NullI18n
